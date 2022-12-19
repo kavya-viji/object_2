ceebb443c492cdf32b634f39fe96b8062a931c1b
Admin <kavyak6999@gmail.com>
2022-12-19 14:20:44 +0530

    object-2

---
+++ object/main.js
@@ -0,0 +1,35 @@
+var biodata =
+{
+name:"kavya",
+age:22,
+nationality:"indian",
+motherToungue:"telugu",
+gender:"female",
+"place-adhi":"chennai",
+hobby:"music",
+character:"softhearted",
+sibling:{bro:"dinesh",sis:"deekshi"},
+favcolor:"purple"
+}
+
+//a = [1,2,3,4];
+
+//delete biodata.age
+//biodata.age=23,
+//biodata.favcolor="pink"
+//console.log(biodata);
+
+//dot notation
+//console.log(biodata.place-adhi)
+
+//bracket notation
+console.log(biodata["place-adhi"])
+
+//for in 
+/*for(adhi in biodata)
+{
+	//console.log(biodata[adhi])
+	console.log(adhi)
+	//console.log(a+":"+biodata[a]);
+ 
+}*/
